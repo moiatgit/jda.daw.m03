63052bc04ce739d6e80c19524c56b26a801e5618
moises <moiatgit@gmail.com>
2018-10-04 17:21:57 +0200

    automated commit on dj oct  4 17:21:57 CEST 2018

---
+++ _downloads/test_py.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+
+# Aquest programa realitza els tests d'un programa en Python
+if [ ${#@} -ne 1 ];
+then
+    echo "Ús: $0 programa.py"
+    echo "$0 redireccionarà tots els fitxers anomenats entrada*.txt contra programa.py i compararà la seva sortida amb el corresponent sortida_esperada*.txt"
+    exit 1
+fi
+
+for fitxer_entrada in entrada*.txt;
+do
+    echo "Comprovant test $fitxer_entrada"
+    fitxer_sortida=sortida_esperada${fitxer_entrada##entrada}
+    if [ ! -f $fitxer_sortida ];
+    then
+        echo "Error: no es troba el fitxer $fitxer_sortida"
+        exit 1
+    fi
+    python3 $1 < $fitxer_entrada | diff - $fitxer_sortida
+done
